400fa5a3f36760b90720a24a447c0efab3a4ecc3
pandasoumen <pandasoumen445@gmail.com>
2024-03-21 22:18:06 +0530

    Added new feature

--- script.js
+++
@@ -1,9 +0,0 @@
-
-// let boardingStation = localStorage(function (boarding_station){});
-
-// getElementby(boardingStation);
-
-// const dayofYear = (date) =>
-//     Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-// dayofYear(new Date());
-// console.log();
